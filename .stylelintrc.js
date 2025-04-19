d9ac151f3a5431a5233fd8d47dc1c6744accb67a
Albert Mírek <albert.mirek@bonami.cz>
2025-04-19 15:43:11 +0200

    fix(lint): stylelint rule overriding import variables

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -2,5 +2,7 @@ module.exports = {
   extends: ["stylelint-config-standard", "stylelint-config-sass-guidelines"],
   rules: {
     "selector-class-pattern": null,
+    // Turn off the import-notation rule completely:
+    "import-notation": null,
   },
 };
