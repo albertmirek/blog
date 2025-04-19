70c8d9fc24b5fa0d7aee4e8f7c505f1ca3a11241
Albert Mírek <albert.mirek@bonami.cz>
2025-04-19 09:38:38 +0200

    feat: add navigation item component

---
+++ src/components/Header/parts/NavItem/index.tsx
@@ -0,0 +1,30 @@
+"use client";
+
+import { FC } from "react";
+import { usePathname } from "next/navigation";
+import Link from "next/link";
+import { classNames } from "@/lib/classNames";
+import * as styles from "./styles.module.scss";
+
+type NavItemProps = {
+  href: string;
+  name: string;
+  className?: string;
+};
+
+export const NavItem: FC<NavItemProps> = (props: NavItemProps) => {
+  const pathName = usePathname();
+
+  return (
+    <Link
+      href={props.href}
+      className={classNames(
+        styles.link,
+        props.href === pathName ? styles.active : "",
+        props.className ? props.className : "",
+      )}
+    >
+      {props.name}
+    </Link>
+  );
+};
