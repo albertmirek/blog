2a3d8c948ac428989ca3269eb70354cc9b854df6
Albert Mírek <albert.mirek@bonami.cz>
2025-04-20 16:02:44 +0200

    feat(component): add image component with proxy source

---
+++ src/ui/ProxyImage/index.tsx
@@ -0,0 +1,22 @@
+import { CSSProperties, FC } from "react";
+import Image from "next/image";
+
+interface Props {
+  width: number;
+  height: number;
+  imageId: string;
+  alt: string;
+  style?: CSSProperties;
+}
+
+export const ProxyImage: FC<Props> = (props: Props) => {
+  return (
+    <Image
+      src={`/api/image/${props.imageId}`}
+      alt={props.alt}
+      width={props.width}
+      height={props.height}
+      style={props.style}
+    />
+  );
+};
