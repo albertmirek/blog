542f4540d7f8b301abb04b33ab7737004e89b4b0
Albert Mírek <albert.mirek@bonami.cz>
2025-04-20 14:23:37 +0200

    feat(page): article dashboard page

--- src/features/articles/pages/DashboardPage/index.tsx
+++ src/features/articles/pages/DashboardPage/index.tsx
@@ -1,10 +1,27 @@
 import { Header } from "@/components/Header";
+import { Heading } from "../../../../ui/Heading";
+import styles from "./styles.module.scss";
+import { ArticleListing } from "@/features/articles/components/ArticleListing";
+import { getArticles } from "@/features/articles/lib/getArticles.server";
+import { getArticleDetail } from "@/features/articles/lib/getArticleDetail.server";
 
-export function DashboardPage() {
+export async function DashboardPage() {
+  const articles = await getArticles();
+  const articleIds = articles.map((_) => _.articleId);
+
+  const articlesWithComments = await Promise.all(
+    articleIds.map((articleId) => getArticleDetail(articleId)),
+  );
+
   return (
     <>
       <Header />
-      <main></main>
+      <main className={styles.wrapper}>
+        <Heading headingLevel={1} headingLevelStyle={1}>
+          Recent articles
+        </Heading>
+        <ArticleListing items={articlesWithComments} />
+      </main>
     </>
   );
 }
