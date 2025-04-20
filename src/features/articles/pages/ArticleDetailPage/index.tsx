e42a7fe3fe6e7590fa2b9a03305f74e054059a0a
Albert Mírek <albert.mirek@bonami.cz>
2025-04-20 16:26:34 +0200

    feat(page): article detail page

--- src/features/articles/pages/ArticleDetailPage/index.tsx
+++ src/features/articles/pages/ArticleDetailPage/index.tsx
@@ -1,15 +1,43 @@
 import { Header } from "@/components/Header";
 
+import styles from "./style.module.scss";
+import { Heading } from "@/ui/Heading";
+import { getArticleDetail } from "@/features/articles/lib/getArticleDetail.server";
+import { format } from "date-fns";
+import { ProxyImage } from "@/ui/ProxyImage";
+import { HtmlRenderer } from "@/ui/HtmlRenderer";
 export async function ArticleDetailPage({
   params,
 }: {
   params: Promise<{ id: string }>;
 }) {
   const { id } = await params;
+  const article = await getArticleDetail(id);
+
   return (
     <>
       <Header />
-      <h1>Article detail page id: ${id}</h1>
+      <div className={styles.wrapper}>
+        <main>
+          <article className={styles.articleWrapper}>
+            <Heading headingLevel={1} headingLevelStyle={1}>
+              {article.title}
+            </Heading>
+            <span className={styles.infoWrapper}>
+              <span>Todo Author</span>
+              <span>•</span>
+              <span>{format(article.createdAt, "MM/dd/yy")}</span>
+            </span>
+            <ProxyImage
+              width={760}
+              height={504}
+              imageId={article.imageId}
+              alt={`${article.title} cover image`}
+            />
+            <HtmlRenderer htmlString={article.content} />
+          </article>
+        </main>
+      </div>
     </>
   );
 }
