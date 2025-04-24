ac0e1e3cf616cda6084c18b1874c95127767321c
Albert Mírek <albert.mirek@bonami.cz>
2025-04-24 22:24:18 +0200

    feat(api): create article

---
+++ src/app/api/articles/route.ts
@@ -0,0 +1,59 @@
+import { NextRequest, NextResponse } from "next/server";
+import { redirect } from "next/navigation";
+import { Routes } from "@/consts/routes";
+import config from "@/config";
+
+type CreateArticleInput = {
+  title: string;
+  perex: string;
+  imageId: string;
+  content: string;
+};
+
+export async function POST(request: NextRequest) {
+  try {
+    const inputs = (await request.json()) as CreateArticleInput;
+    if (
+      !inputs.title ||
+      inputs.title === "" ||
+      !inputs.perex ||
+      inputs.perex === "" ||
+      !inputs.imageId ||
+      inputs.imageId === "" ||
+      !inputs.content ||
+      inputs.content === ""
+    ) {
+      return NextResponse.json(
+        { error: "One of required fields were not filled" },
+        { status: 400 },
+      );
+    }
+
+    const res = await fetch(`${config.apiEndpoint}/articles`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "X-API-KEY": config.apiKey,
+      },
+      body: JSON.stringify(inputs),
+    });
+    if (res.status === 403) {
+      redirect(`${Routes.LOGIN}?missingToken=1`);
+    }
+
+    if (!res.ok) {
+      return NextResponse.json(
+        { error: "There was an error with creating article" },
+        { status: res.status },
+      );
+    }
+
+    redirect(Routes.DASHBOARD);
+  } catch (error) {
+    console.error(error);
+    return NextResponse.json(
+      { error: "Something went wrong" },
+      { status: 500 },
+    );
+  }
+}
