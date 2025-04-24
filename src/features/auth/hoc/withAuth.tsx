09541565b305c1a66333470c3fceb515e75fe54e
Albert Mírek <albert.mirek@bonami.cz>
2025-04-24 22:23:41 +0200

    refactor: routes

--- src/features/auth/hoc/withAuth.tsx
+++ src/features/auth/hoc/withAuth.tsx
@@ -1,6 +1,7 @@
 import { ReactNode } from "react";
 import { cookies } from "next/headers";
 import { redirect } from "next/navigation";
+import { Routes } from "@/consts/routes";
 
 export function withAuth<P extends Record<string, unknown>>(
   PageComponent: (props: P) => ReactNode | Promise<ReactNode>,
@@ -8,7 +9,7 @@ export function withAuth<P extends Record<string, unknown>>(
   return async (props: P) => {
     const token = (await cookies()).get("accessToken")?.value;
     if (!token || token === "") {
-      redirect("/login?missingToken=1");
+      redirect(`${Routes.LOGIN}?missingToken=1`);
     }
     return PageComponent({ ...(props as P) });
   };
